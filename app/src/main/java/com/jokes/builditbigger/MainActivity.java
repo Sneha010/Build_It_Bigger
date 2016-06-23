6dd23ac5d32c9234e72d33f503fb4f29dab70d82
Sneha010 <sneha.khadatare@gmail.com>
2016-06-23 13:14:11 +0530

    Initial commit

---
+++ app/src/main/java/com/jokes/builditbigger/MainActivity.java
@@ -0,0 +1,18 @@
+package com.jokes.builditbigger;
+
+import android.content.Context;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Pair;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
+    }
+}
