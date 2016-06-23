69944edc912c8f46b48df96c713e70510ab6954f
Sneha010 <sneha.khadatare@gmail.com>
2016-06-23 17:58:16 +0530

    paid and free Flavours added

---
+++ app/src/free/java/com/jokes/builditbigger/MainActivityFragment.java
@@ -0,0 +1,53 @@
+package com.jokes.builditbigger;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class MainActivityFragment extends Fragment {
+
+
+    @Bind(R.id.adView)
+    AdView mAdView;
+
+    public static MainActivityFragment newInstance() {
+        MainActivityFragment fragment = new MainActivityFragment();
+        return fragment;
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ButterKnife.bind(this, view);
+
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .build();
+        mAdView.loadAd(adRequest);
+
+
+        return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        ButterKnife.unbind(this);
+    }
+}
--- app/src/main/java/com/jokes/builditbigger/MainActivity.java
+++ app/src/main/java/com/jokes/builditbigger/MainActivity.java
@@ -2,15 +2,37 @@ package com.jokes.builditbigger;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 
 public class MainActivity extends AppCompatActivity {
+
+
+    @Bind(R.id.main_container)
+    LinearLayout mMainContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
 
         //new EndpointsAsyncTask().execute(new Pair<Context, String>(this, "Manfred"));
+
+        getSupportFragmentManager().beginTransaction()
+                .add(R.id.main_container , MainActivityFragment.newInstance())
+                .commit();
+
+
     }
+
+    public void tellJoke(View view){
+        Toast.makeText(this, "derp", Toast.LENGTH_SHORT).show();
+    }
+
 }
